3fa0f890a4c0f52923a3abc2d88bfb37e3abb6ef
r5k0y2k5k9r6 <57274232+r5k0y2k5k9r6@users.noreply.github.com>
2020-04-18 18:49:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+jQuery(function($){
+  $('.slider').each(function(){
+  $(this).slick({
+    autoplay:true,
+    // autoplaySpeed:1000,
+    dots:true,
+    arrows:false,
+    centerMode: true,
+    centerPadding: '25%',
+  });
+});	
+});	
